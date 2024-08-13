e28d344231b2489f4fc07aaef7abcdc80994ddd9
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-08-13 21:16:37 +0530

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -3,12 +3,12 @@ import subprocess
 
 # Step 1: Capture Face Images
 print("\n[INFO] Starting Face Capture...")
-subprocess.run(["python", "face_capture.py"])
+subprocess.run(["python3", "image_taker.py"])
 
 # Step 2: Train the Model
 print("\n[INFO] Training the Model...")
-subprocess.run(["python", "train_model.py"])
+subprocess.run(["python3", "face_train.py"])
 
 # Step 3: Recognize Faces
 print("\n[INFO] Starting Face Recognition...")
-subprocess.run(["python", "recognize_faces.py"])
+subprocess.run(["python3", "face_recognizer.py"])
